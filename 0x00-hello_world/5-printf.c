93a1c18f442267e30a8d48b1c201570afa40462f
entonuina <entonuina61@gmail.com>
2023-08-14 16:30:00 -0700

    print content using the print function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main -ith proper grammar, but the outcome is a piece of art
+ *Return :0 (success)
+ */
+int main(void)
+{
+printf("ith proper grammar, but the outcome is a piece of art\n");
+return (0);
+}
